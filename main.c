6f0b1287f0af406c4f096f781fe37ae84945d2d3
Christiano Haesbaert <haesbaert@haesbaert.org>
2023-06-27 10:25:46 +0200

    errx

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
+
+static void
+errx(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+	printf("\n");
+
+	exit(1);
+}
+
 
 int
 main(int __unused, char *__unused[])
